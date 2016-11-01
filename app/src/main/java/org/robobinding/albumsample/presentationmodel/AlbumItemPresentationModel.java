f3d19858a75b199286a36c5bcb135f76052e918b
weicheng113 <weicheng112@gmail.com>
2016-11-01 11:52:06 +1100

    Upgraded Android Gradle build tool to 2.2.1.

--- app/src/main/java/org/robobinding/albumsample/presentationmodel/AlbumItemPresentationModel.java
+++ app/src/main/java/org/robobinding/albumsample/presentationmodel/AlbumItemPresentationModel.java
@@ -1,5 +1,7 @@
 package org.robobinding.albumsample.presentationmodel;
 
+import android.util.Log;
+
 import org.robobinding.albumsample.model.Album;
 import org.robobinding.itempresentationmodel.ItemContext;
 import org.robobinding.itempresentationmodel.ItemPresentationModel;
@@ -12,6 +14,7 @@ public class AlbumItemPresentationModel implements ItemPresentationModel<Album> 
     protected Album album;
 
     public String getTitle() {
+        Log.d(AlbumItemPresentationModel.class.getSimpleName(), "in getTitle");
         return album.getTitle();
     }
 
@@ -21,6 +24,7 @@ public class AlbumItemPresentationModel implements ItemPresentationModel<Album> 
 
     @Override
     public void updateData(Album bean, ItemContext itemContext) {
+        Log.d(AlbumItemPresentationModel.class.getSimpleName(), "in updateData");
         this.album = bean;
     }
 }
